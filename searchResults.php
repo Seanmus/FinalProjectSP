ee4a9e8f25dc285c4e9ed958c62a91716dc72783
Sean Piche <seanpiche718@gmail.com>
2020-12-09 02:26:25 -0600

    Category Search
    Users can now search and specify the search to a specific category.

--- searchResults.php
+++ searchResults.php
@@ -5,11 +5,30 @@
     $name = "%".$name."%";
 
 
-    $query = "SELECT * FROM games WHERE UPPER(name) LIKE UPPER(:name)";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':name', $name);
-    $statement->execute();
+    $console = filter_input(INPUT_POST, 'console', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
+    if(!empty($console)){
+      $query = "SELECT * FROM games WHERE UPPER(name) LIKE UPPER(:name) AND platform = :platform";
+      $statement = $db->prepare($query);
+      $statement->bindValue(':name', $name);
+      $statement->bindValue(':platform', $console);
+      $statement->execute();
+  
+      $query = "SELECT * FROM consoles";
+      $statementCategories = $db->prepare($query);
+      $statementCategories->execute();
+    }
+    else{
+      $query = "SELECT * FROM games WHERE UPPER(name) LIKE UPPER(:name)";
+      $statement = $db->prepare($query);
+      $statement->bindValue(':name', $name);
+      $statement->execute();
+  
+      $query = "SELECT * FROM consoles";
+      $statementCategories = $db->prepare($query);
+      $statementCategories->execute();
+    }
+    
 ?>
 
 <!DOCTYPE html>
@@ -26,6 +45,20 @@
     <?php while($row = $statement->fetch()): ?>
     <div class="game_post">
       <h2><a href="show.php?id=<?=$row['id'] ?>"><?=$row['name']?></a></h2>
+      <form id = "game" action="searchResults.php" method="post">
+
+      <label for ="console"> Choose a Platform: </label>
+      <select name="console" id="platform">
+        <option value="">No Console</option>
+      <?php while($rowCategory = $statementCategories->fetch()): ?>
+        <option value="<?=$rowCategory['console']?>"><?=$rowCategory['console']?></option>
+      <?php endwhile ?>
+      </select>
+
+      <input type="hidden" name="title" value="<?=$name?>" />
+
+      <button type="submit">Submit Search</button>
+</form>
     </div>
     <?php endwhile ?>
 <?php else: ?>
